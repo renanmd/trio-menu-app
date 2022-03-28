6f49e96e91b69e86fe279334b91c7320c1d96d51
Renan Muller <renanmd@gmail.com>
2022-03-28 19:37:09 -0300

    First commit.

---
+++ Podfile
@@ -0,0 +1,6 @@
+target 'trio-menu-app' do
+    pod 'Alamofire', '~> 5.0.0-rc.2'
+    pod 'SVProgressHUD', '~> 2.2'
+  use_frameworks!
+
+end
---
+++ Pods/Target Support Files/Pods-trio-menu-app/Pods-trio-menu-app-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_trio_menu_app : NSObject
+@end
+@implementation PodsDummy_Pods_trio_menu_app
+@end
